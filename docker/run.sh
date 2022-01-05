77ceff17f5d5869fec146d33020753a9073d07b0
santos <ronireiosantos@gmail.com>
2022-01-05 22:07:48 +0300

    - disable use of nginx

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-if [[ $ENV == 'local' ]]; then
-	echo "ENV LOCAL -------------------"
-	python manage.py makemigrations
-	python manage.py migrate --noinput
-	python manage.py loaddata api/fixtures/*.json
-	python manage.py collectstatic --noinput
-	uvicorn api.asgi:application --reload --use-colors --host 0.0.0.0 --port 8000
-else
-	cp /usr/src/app/nginx.conf /etc/nginx/conf.d/default.conf
-	cp /usr/src/app/supervisor.conf /etc/supervisor/conf.d/
-	supervisord -n
-fi
+#if [[ $ENV == 'local' ]]; then
+#	echo "ENV LOCAL -------------------"
+#	python manage.py makemigrations
+#	python manage.py migrate --noinput
+#	python manage.py loaddata api/fixtures/*.json
+#	python manage.py collectstatic --noinput
+#	uvicorn api.asgi:application --reload --use-colors --host 0.0.0.0 --port 8000
+#else
+#	cp /usr/src/app/nginx.conf /etc/nginx/conf.d/default.conf
+#	cp /usr/src/app/supervisor.conf /etc/supervisor/conf.d/
+#	supervisord -n
+#fi
+
+echo "Running:: ENV $ENV -------------------"
+python manage.py makemigrations
+python manage.py migrate --noinput
+python manage.py loaddata api/fixtures/*.json
+python manage.py collectstatic --noinput
+uvicorn api.asgi:application --reload --use-colors --host 0.0.0.0 --port 8000
